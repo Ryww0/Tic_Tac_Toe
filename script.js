e6a2a77d33dd17ca115212cc6912ea58c98e087c
Ryww0 <laurent.chaitas@gmail.com>
2022-06-22 15:30:21 +0200

    function win (8 conditions) created

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // index to count if it's player 1 turn or not
 let i = 0;
 const cases = document.querySelectorAll(".case");
+const resultText = document.getElementById("result");
 
 function turn() {
   //new index, new turn
@@ -15,10 +16,82 @@ function turn() {
 
 // add crosses and cirle in the cases
 function play() {
-  this.innerHTML = turn();
+  // console.log(i);
+  if (!this.innerHTML) {
+    this.innerHTML = turn();
+    win();
+  }
 }
 
 //change the content cases on click
 cases.forEach((c) => {
   c.addEventListener("click", play);
 });
+
+//8 victory conditions
+function win() {
+  //1st win condition
+  if (
+    cases[0].innerHTML !== "" &&
+    cases[0].innerHTML === cases[1].innerHTML &&
+    cases[0].innerHTML === cases[2].innerHTML
+  ) {
+    resultText.innerHTML = "You kicked his ass !";
+  }
+  //2nd win cond
+  if (
+    cases[3].innerHTML !== "" &&
+    cases[3].innerHTML === cases[4].innerHTML &&
+    cases[3].innerHTML === cases[5].innerHTML
+  ) {
+    resultText.innerHTML = "You kicked his ass !";
+  }
+  //3rd win cond
+  if (
+    cases[6].innerHTML !== "" &&
+    cases[6].innerHTML === cases[7].innerHTML &&
+    cases[6].innerHTML === cases[8].innerHTML
+  ) {
+    resultText.innerHTML = "You kicked his ass !";
+  }
+  //4th win cond
+  if (
+    cases[0].innerHTML !== "" &&
+    cases[0].innerHTML === cases[3].innerHTML &&
+    cases[0].innerHTML === cases[5].innerHTML
+  ) {
+    resultText.innerHTML = "You kicked his ass !";
+  }
+  //5th win cond
+  if (
+    cases[1].innerHTML !== "" &&
+    cases[1].innerHTML === cases[4].innerHTML &&
+    cases[1].innerHTML === cases[7].innerHTML
+  ) {
+    resultText.innerHTML = "You kicked his ass !";
+  }
+  //6th win cond
+  if (
+    cases[2].innerHTML !== "" &&
+    cases[2].innerHTML === cases[5].innerHTML &&
+    cases[2].innerHTML === cases[8].innerHTML
+  ) {
+    resultText.innerHTML = "You kicked his ass !";
+  }
+  //7th win cond
+  if (
+    cases[2].innerHTML !== "" &&
+    cases[2].innerHTML === cases[4].innerHTML &&
+    cases[2].innerHTML === cases[6].innerHTML
+  ) {
+    resultText.innerHTML = "You kicked his ass !";
+  }
+  //8th win cond
+  if (
+    cases[0].innerHTML !== "" &&
+    cases[0].innerHTML === cases[4].innerHTML &&
+    cases[0].innerHTML === cases[8].innerHTML
+  ) {
+    resultText.innerHTML = "You kicked his ass !";
+  }
+}
